c43fab6edd9156bb2e359c49116b84e7989eb6cf
Patrick Kuijpers <PKuijpers@dsw.nl>
2016-06-21 22:18:16 +0200

    Verbetering code Storage

--- mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Manager/Storage.java
+++ mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Manager/Storage.java
@@ -9,20 +9,22 @@ import java.io.IOException;
 import nl.tcilegnar.dndcharactersheet.App;
 
 public class Storage {
-	private static final String SETTINGS_EXP = "Experience";
-	private static final int PREF_NOT_FOUND = -1;
 	public static final int DEF_VALUE = 0;
+
+	public enum Settings {
+		Experience
+	}
 
 	public enum Key {
 		CURRENT_EXP
 	}
 
-	public enum Name {
+	public enum FileName {
 		TEST_FILE
 	}
 
 	public static void saveSharedPreference(Key key, int value) {
-		SharedPreferences settings = App.getContext().getSharedPreferences(SETTINGS_EXP, 0);
+		SharedPreferences settings = App.getContext().getSharedPreferences(Settings.Experience.name(), 0);
 		SharedPreferences.Editor editor = settings.edit();
 		editor.putInt(key.name(), value);
 
@@ -31,19 +33,19 @@ public class Storage {
 	}
 
 	public static int getSharedPreference(Key key) {
-		SharedPreferences settings = App.getContext().getSharedPreferences(SETTINGS_EXP, 0);
+		SharedPreferences settings = App.getContext().getSharedPreferences(Settings.Experience.name(), 0);
 		return settings.getInt(key.name(), DEF_VALUE);
 	}
 
 	public static void saveFileDataTest() throws IOException {
-		int homeScore;
-		byte[] homeScoreBytes;
+		int homeScore = 0;
+		byte[] homeScoreBytes = new byte[0];
 
 		homeScoreBytes[0] = (byte) homeScore;
 		homeScoreBytes[1] = (byte) (homeScore >> 8);
 		homeScoreBytes[2] = (byte) (homeScore >> 16);
 
-		FileOutputStream outputStream = App.getContext().openFileOutput(Name.TEST_FILE.name(), Context.MODE_PRIVATE);
+		FileOutputStream outputStream = App.getContext().openFileOutput(FileName.TEST_FILE.name(), Context.MODE_PRIVATE);
 		outputStream.write(homeScoreBytes);
 		outputStream.close();
 	}
