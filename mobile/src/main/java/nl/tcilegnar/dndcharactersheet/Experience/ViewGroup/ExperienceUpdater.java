f62dce78f2eb20a48ed84f66bd50b9ed286a6627
Patrick Kuijpers <PKuijpers@dsw.nl>
2016-06-25 20:39:05 +0200

    Code netter gemaakt

--- mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Experience/ViewGroup/ExperienceUpdater.java
+++ mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Experience/ViewGroup/ExperienceUpdater.java
@@ -4,28 +4,28 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import nl.tcilegnar.dndcharactersheet.Experience.View.ExperienceInput;
 import nl.tcilegnar.dndcharactersheet.Experience.View.ExperiencePicker;
 import nl.tcilegnar.dndcharactersheet.R;
 
-public class ExperienceUpdater extends FrameLayout implements OnClickListener {
+public class ExperienceUpdater extends LinearLayout implements OnClickListener {
 	private ExperiencePicker expPicker;
 	private ExperienceInput expInput;
 
 	private ExperienceUpdateListener experienceUpdateListener;
 
 	public ExperienceUpdater(Context context, AttributeSet attrs) {
-		super(context, attrs);
+		super(context, attrs, R.attr.expUpdaterStyle);
 		init(context);
 	}
 
 	private void init(Context context) {
-		View view = inflate(context, R.layout.experience_updater, this);
+		inflate(context, R.layout.experience_updater, this);
 
-		expPicker = (ExperiencePicker) view.findViewById(R.id.experience_picker);
-		expInput = (ExperienceInput) view.findViewById(R.id.experience_input);
+		expPicker = (ExperiencePicker) findViewById(R.id.experience_picker);
+		expInput = (ExperienceInput) findViewById(R.id.experience_input);
 
 		(findViewById(R.id.experience_plus_button)).setOnClickListener(this);
 		(findViewById(R.id.experience_min_button)).setOnClickListener(this);
@@ -42,12 +42,11 @@ public class ExperienceUpdater extends FrameLayout implements OnClickListener {
 	@Override
 	public void onClick(View view) {
 		int viewId = view.getId();
-		int expUpdateValue = getExpValue();
 		if (viewId == R.id.experience_plus_button) {
-			experienceUpdateListener.onUpdateExperience(expUpdateValue);
+			experienceUpdateListener.onUpdateExperience(getExpValue());
 		}
 		if (viewId == R.id.experience_min_button) {
-			experienceUpdateListener.onUpdateExperience(-expUpdateValue);
+			experienceUpdateListener.onUpdateExperience(-getExpValue());
 		}
 	}
 
