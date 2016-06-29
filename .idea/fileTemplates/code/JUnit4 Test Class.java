fb4f0936bb96cfd7e00383fe13e75ebb9ea2e22b
Patrick Kuijpers <PKuijpers@dsw.nl>
2016-06-29 23:51:36 +0200

    FileTemplate updated

--- .idea/fileTemplates/code/JUnit4 Test Class.java
+++ .idea/fileTemplates/code/JUnit4 Test Class.java
@@ -2,6 +2,8 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import nl.tcilegnar.dndcharactersheet.BuildConfig;
+
 import static org.junit.Assert.*;
 #parse("File Header.java")
 @RunWith(RobolectricGradleTestRunner.class)
