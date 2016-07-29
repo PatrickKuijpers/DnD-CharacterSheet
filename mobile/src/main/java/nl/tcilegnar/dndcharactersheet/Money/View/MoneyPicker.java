ca0990cdd328b480d94d2959e293dadf29bd5fbd
Patrick Kuijpers <PKuijpers@dsw.nl>
2016-07-29 14:19:38 +0200

    Classes & styles samentrekken: money numberpicker(slider)

--- mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Money/View/MoneyPicker.java
+++ mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Money/View/MoneyPicker.java
@@ -1,97 +1,18 @@
 package nl.tcilegnar.dndcharactersheet.Money.View;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.os.Parcelable;
+import android.support.annotation.VisibleForTesting;
 import android.util.AttributeSet;
-import android.view.View;
-import android.widget.NumberPicker;
 
-import nl.tcilegnar.dndcharactersheet.Money.ViewGroup.MoneyView;
 import nl.tcilegnar.dndcharactersheet.Storage.Settings;
 
-public class MoneyPicker extends NumberPicker {
-    private static final int MIN_VALUE = 0;
-    private static final int MAX_VALUE = 99;
-    private Settings settings;
-    private MoneyView moneyView;
-
-    public enum SavedValues {
-        SAVED_INSTANCE, CURRENT_PICKER_INDEX
-    }
-
+public class MoneyPicker extends MoneyBaseNumberPicker {
     public MoneyPicker(Context context, AttributeSet attrs) {
-        this(context, attrs, Settings.getInstance());
-    }
-
-    public MoneyPicker(Context context, AttributeSet attrs, Settings settings) {
         super(context, attrs);
-        this.settings = settings;
-        initViewsIfVisible();
     }
 
-    private void initViewsIfVisible() {
-        //        boolean shouldBeVisible = settings.;
-        boolean shouldBeVisible = true;
-        if (shouldBeVisible) {
-            init();
-            setVisibility(View.VISIBLE);
-        } else {
-            setVisibility(View.GONE);
-        }
-    }
-
-    private void init() {
-        initPickerValues();
-    }
-
-    private void initPickerValues() {
-        String[] displayedValues = generateDisplayedValues();
-        super.setDisplayedValues(displayedValues);
-        super.setMinValue(MIN_VALUE);
-        super.setMaxValue(displayedValues.length - 1);
-        super.setValue(0);
-    }
-
-    private String[] generateDisplayedValues() {
-        int pickerStepSize = 1; //settings.getExperiencePickerStepSize();
-        int numberOfSteps = ((MAX_VALUE - MIN_VALUE) / pickerStepSize) + 1;
-        String[] experienceValues = new String[numberOfSteps];
-        int nextValue = MIN_VALUE;
-        for (int i = 0; i < experienceValues.length; i++) {
-            experienceValues[i] = String.valueOf(nextValue);
-            nextValue = nextValue + pickerStepSize;
-        }
-        return experienceValues;
-    }
-
-    @Override
-    public Parcelable onSaveInstanceState() {
-        Bundle bundle = new Bundle();
-        bundle.putParcelable(SavedValues.SAVED_INSTANCE.name(), super.onSaveInstanceState());
-        bundle.putInt(SavedValues.CURRENT_PICKER_INDEX.name(), getValue());
-        return bundle;
-    }
-
-    @Override
-    public void onRestoreInstanceState(Parcelable state) {
-        if (state instanceof Bundle) {
-            Bundle bundle = (Bundle) state;
-            setValue(bundle.getInt(SavedValues.CURRENT_PICKER_INDEX.name()));
-            state = bundle.getParcelable(SavedValues.SAVED_INSTANCE.name());
-        }
-        super.onRestoreInstanceState(state);
-    }
-
-    public void updateSettingsData() {
-        initViewsIfVisible();
-    }
-
-    public int getCurrentSelectedMoneyValue() {
-        return Integer.valueOf(getDisplayedValues()[getValue()]);
-    }
-
-    public void setMoneyValue(int moneyValue) {
-        setValue(moneyValue);
+    @VisibleForTesting
+    public MoneyPicker(Context context, AttributeSet attrs, Settings settings) {
+        super(context, attrs, settings);
     }
 }
