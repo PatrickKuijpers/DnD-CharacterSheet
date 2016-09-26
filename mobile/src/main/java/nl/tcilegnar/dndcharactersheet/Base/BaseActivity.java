c129a4ed3c6d22586819249bb7869477e0ac2fbb
Patrick Kuijpers <PKuijpers@dsw.nl>
2016-09-26 23:01:12 +0200

    Melding verduidelijkt

--- mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Base/BaseActivity.java
+++ mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Base/BaseActivity.java
@@ -64,9 +64,8 @@ public abstract class BaseActivity extends AppCompatActivity implements OnNaviga
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // Handle action bar item clicks here. The action bar will automatically handle clicks on the Home/Up button,
+        // so long as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
