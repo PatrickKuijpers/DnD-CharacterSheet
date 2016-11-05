77184e4a7e9341c10d77a3bfea81d91e7e95686a
Patrick Kuijpers <PKuijpers@dsw.nl>
2016-11-05 15:02:00 +0100

    Voer ook repearting animaties uit voor meer dan 1 level up / down

--- mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Experience/Animations/ExperienceProgressBarAnimation.java
+++ mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Experience/Animations/ExperienceProgressBarAnimation.java
@@ -15,19 +15,17 @@ public enum ExperienceProgressBarAnimation implements AnimationListener {
     private float startProgress;
     private float finalProgress;
     private int numberOfLevelsChanged;
-    private int startMaxProgress;
     private int newMaxProgress;
 
     private Experience experience;
 
-    private boolean onProgressMaxReached;
-
-    private boolean finalAnimationStarted = false;
+    private boolean repeatingAnimationsEnded = false;
     private boolean finalAnimationEnded = true;
+    private int repeatsLeft = 0;
 
     public void start(ProgressBar progressBar, Experience experience, int numberOfLevelsChanged) {
         init(progressBar, experience, numberOfLevelsChanged);
-        performInitialAnimation(progressBar, numberOfLevelsChanged);
+        performInitialAnimation();
     }
 
     private void init(ProgressBar progressBar, Experience experience, int numberOfLevelsChanged) {
@@ -36,39 +34,71 @@ public enum ExperienceProgressBarAnimation implements AnimationListener {
         this.startProgress = progressBar.getProgress();
         this.finalProgress = experience.getCurrentExp();
         this.numberOfLevelsChanged = numberOfLevelsChanged;
-        this.startMaxProgress = progressBar.getMax();
         this.newMaxProgress = experience.getMax();
 
         this.experience = experience;
 
-        finalAnimationStarted = false;
+        int repeats = getNumberOfRepeatingAnimations(numberOfLevelsChanged);
+        this.repeatsLeft = repeats;
+        repeatingAnimationsEnded = false;
         finalAnimationEnded = false;
     }
 
-    private void performInitialAnimation(ProgressBar progressBar, int numberOfLevelsChanged) {
+    private int getNumberOfRepeatingAnimations(int numberOfLevelsChanged) {
+        if (numberOfLevelsChanged > 1) {
+            return numberOfLevelsChanged - 1;
+        } else if (numberOfLevelsChanged < -1) {
+            return numberOfLevelsChanged + 1;
+        } else { // numberOfLevelsChanged == -1 || 0 || 1
+            return 0;
+        }
+    }
+
+    private void performInitialAnimation() {
         if (numberOfLevelsChanged > 0) {
             startAnimation(startProgress, progressBar.getMax());
         } else if (numberOfLevelsChanged < 0) {
             startAnimation(startProgress, 0);
         } else { // numberOfLevelsChanged == 0
             startAnimation(startProgress, finalProgress);
-            finalAnimationStarted = true;
+        }
+    }
+
+    private void performRepeatingAnimation() {
+        if (repeatsLeft > 0) {
+            repeatsLeft--;
+            startAnimationFromMinimumProgressTo(progressBar.getMax());
+        } else if (repeatsLeft < 0) {
+            repeatsLeft++;
+            startAnimationFromMaximumProgressTo(0);
+        } else { // repeatsLeft == 0
+            repeatingAnimationsEnded = true;
+            performFinalAnimation();
         }
     }
 
     private void performFinalAnimation() {
         if (numberOfLevelsChanged > 0) {
             progressBar.setMax(newMaxProgress);
-            progressBar.setProgress(0);
-            startAnimation(0, finalProgress);
+            startAnimationFromMinimumProgressTo(finalProgress);
         } else if (numberOfLevelsChanged < 0) {
             progressBar.setMax(newMaxProgress);
-            progressBar.setProgress(progressBar.getMax());
-            startAnimation(progressBar.getMax(), finalProgress);
+            startAnimationFromMaximumProgressTo(finalProgress);
         } else { // numberOfLevelsChanged == 0
             // Hier hoeft niets meer te gebeuren
         }
-        finalAnimationStarted = true;
+    }
+
+    private void startAnimationFromMinimumProgressTo(float to) {
+        int from = 0;
+        progressBar.setProgress(from);
+        startAnimation(from, to);
+    }
+
+    private void startAnimationFromMaximumProgressTo(float to) {
+        float from = progressBar.getMax();
+        progressBar.setProgress((int) from);
+        startAnimation(from, to);
     }
 
     private void startAnimation(float from, float to) {
@@ -84,8 +114,8 @@ public enum ExperienceProgressBarAnimation implements AnimationListener {
 
     @Override
     public void onAnimationEnd(Animation animation) {
-        if (!finalAnimationStarted) {
-            performFinalAnimation();
+        if (!repeatingAnimationsEnded) {
+            performRepeatingAnimation();
         } else {
             finalAnimationEnded = true;
         }
