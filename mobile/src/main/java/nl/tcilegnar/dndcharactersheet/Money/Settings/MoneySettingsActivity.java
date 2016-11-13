9170f2fbe1688ced2570eb11a05376bb8d577135
Patrick Kuijpers <PKuijpers@dsw.nl>
2016-11-13 19:39:14 +0100

    Kleine wijziging MoneySettings type

--- mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Money/Settings/MoneySettingsActivity.java
+++ mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Money/Settings/MoneySettingsActivity.java
@@ -3,13 +3,11 @@ package nl.tcilegnar.dndcharactersheet.Money.Settings;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-import nl.tcilegnar.dndcharactersheet.Base.Settings.SettingsFragment;
-
 public class MoneySettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SettingsFragment settingsFragment = new MoneySettingsFragment();
+        MoneySettingsFragment settingsFragment = new MoneySettingsFragment();
         getFragmentManager().beginTransaction().replace(android.R.id.content, settingsFragment).commit();
     }
 }
