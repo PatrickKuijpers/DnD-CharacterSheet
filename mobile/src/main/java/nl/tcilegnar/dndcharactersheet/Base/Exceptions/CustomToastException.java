dfec564b10a6bcc8690446b9eaaf5ad5626101e5
Patrick Kuijpers <PKuijpers@dsw.nl>
2016-11-13 12:44:01 +0100

    Fix op vorige commit

--- mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Base/Exceptions/CustomToastException.java
+++ mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Base/Exceptions/CustomToastException.java
@@ -2,12 +2,9 @@ package nl.tcilegnar.dndcharactersheet.Base.Exceptions;
 
 import android.widget.Toast;
 
-import nl.tcilegnar.dndcharactersheet.App;
-
 public abstract class CustomToastException extends Exception {
     public CustomToastException(String message) {
         super(message);
-        SingleToast.INSTANCE.show(App.getContext(), getMessage(), Toast.LENGTH_LONG);
-        SingleToast.INSTANCE.show(App.getContext(), "some other message", Toast.LENGTH_LONG);
+        SingleToast.INSTANCE.show(getMessage(), Toast.LENGTH_LONG);
     }
 }
