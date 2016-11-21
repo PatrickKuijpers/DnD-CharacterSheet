0d22bdeeb8e8179e30d5781031a43ef198b3fee9
Patrick Kuijpers <PKuijpers@dsw.nl>
2016-11-21 23:56:58 +0100

    Vertalingen EN & NL

--- mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Base/BaseActivity.java
+++ mobile/src/main/java/nl/tcilegnar/dndcharactersheet/Base/BaseActivity.java
@@ -40,7 +40,7 @@ public abstract class BaseActivity extends AppCompatActivity implements OnNaviga
     private void initDrawerMenu(Toolbar toolbar) {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string
-                .navigation_drawer_open, R.string.navigation_drawer_close);
+                .content_description_navigation_drawer_open, R.string.content_description_navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
