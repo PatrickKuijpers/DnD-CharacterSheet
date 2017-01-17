aceea66eba138a3d810dd3cf5262936ff584aad6
Patrick Kuijpers <patrickuijpers@gmail.com>
2017-01-17 21:01:46 +0100

    Pathfinder flavor toegevoegd

---
+++ mobile/src/testPathfinder/java/nl/tcilegnar/dndcharactersheet/Money/MoneyCalculatorTestPathfinder.java
@@ -0,0 +1,12 @@
+package nl.tcilegnar.dndcharactersheet.Money;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import nl.tcilegnar.dndcharactersheet.BuildConfig;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MoneyCalculatorTestPathfinder extends MoneyCalculatorTest {
+}
